1b4fee32130e020c52930421b50f1c1c8ca2009e
skorani <samira.korani@gmail.com>
2020-02-10 06:19:37 +0000

    change database style

--- lookup_dic.py
+++ lookup_dic.py
@@ -1,34 +1,15 @@
-import db
 
 reveresd_array = []
 lookup_reverse_dic_CODE = {}
 lookup_dic_CODE = {}
 
 
-def init():
-    db.connect()
-
-
-def release():
-    db.close()
 
 def create_dic_code():
     global lookup_reverse_dic_CODE
     global lookup_dic_CODE
-
-    cursor = db.cursor(buffered=True)
-    cursor.execute("SET NAMES utf8;")  # or utf8 or any other charset you want to handle
-    cursor.execute("SET CHARACTER SET utf8;")  # same as above
-    cursor.execute("SET character_set_connection=utf8;")
-
-
-    sql = "SELECT replace_with, phrase  FROM phrase;"
-    # Execute the SQL command
-    cursor.execute(sql)
-    # Fetch all the rows in a list of lists.
-    reveresd_array = cursor.fetchall()
-
-    for row in reveresd_array:
+    with open ('phrase.csv') as  reveresd_array:
+     for row in reveresd_array:
         replace_with, phrase = row
         # Now print fetched result
         lookup_dic_CODE.update({replace_with: [phrase]})
