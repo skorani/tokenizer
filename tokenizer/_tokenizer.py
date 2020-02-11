8e07e27894486557beed98e3b6c934d23b8d4a27
skorani <samira.korani@gmail.com>
2020-02-11 22:13:54 +0000

    pattern variable removed

--- tokenizer/_tokenizer.py
+++ tokenizer/_tokenizer.py
@@ -1,33 +1,29 @@
 from flashtext import KeywordProcessor
-
-from . import lookup_dic
+import lookup_dic
 
 
 def init():
     global __keyword_processor
     global __reverse_lookup_dict
     global lookup_dic_CODE
-    global pattern
 
-    lookup_dic.read_phrases()
+    lookup_dic.init()
     __keyword_processor = KeywordProcessor()
     __keyword_processor.add_keywords_from_dict(lookup_dic.lookup_dic_CODE)
 
-#    __reverse_lookup_dict = lookup_dic.create_reverse_dic_code()
-
+    #__reverse_lookup_dict = lookup_dic.create_reverse_dic_code()
+    lookup_dic.release()
 
 def tokenize(text):
     global __keyword_processor
     global __reverse_lookup_dict
-    global pattern
+    #print(text)
     text = __keyword_processor.replace_keywords(text)
+    #print(text)
+    #print('###')
 
     tokens_list = text.split()
 
-    tokens_list = [
-        lookup_dic.reverse_replace(token)
-        if token in lookup_dic.lookup_dic_CODE else token
-        for token in tokens_list
-    ]
+    tokens_list = [lookup_dic.lookup_dic_CODE[token] if token in lookup_dic.lookup_dic_CODE.keys() else token for token in tokens_list]
 
     return tokens_list
